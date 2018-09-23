11b7dc15a3aa1a8351afb0e832583038d417b9e3
Dima Sazhin <dimasazhin@ugcs.com>
2018-09-23 20:16:24 +0300

    Fix actions

--- app/components/new_quote.js
+++ app/components/new_quote.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Dimensions, Text, TextInput, TouchableOpacity} from 'react-native';
 
 import { connect } from 'react-redux';
-import { addQuote, updateQuote } from '../actions'
+import { addQuote, updateQuote } from '../actions/'
 import { Actions } from 'react-native-router-flux';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 
